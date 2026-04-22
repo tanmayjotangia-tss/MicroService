f3e63aba78a6d630f4aa77688be3e6b6ba1f7955
tanmayjotangia-tss <tanmay.jotangia@tssconsultancy.com>
2026-04-22 15:44:01 +0530

    Added Eureka Configurations

---
+++ service-registory/src/test/java/com/techlab/service_registory/ServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techlab.service_registory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
