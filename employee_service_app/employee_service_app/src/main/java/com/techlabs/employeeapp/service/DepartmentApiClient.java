e355e6e2fe98871a90518f90f3199154b584383b
tanmayjotangia-tss <tanmay.jotangia@tssconsultancy.com>
2026-04-22 16:17:25 +0530

    Load Balancing Implemented using Eureka

--- employee_service_app/employee_service_app/src/main/java/com/techlabs/employeeapp/service/DepartmentApiClient.java
+++ employee_service_app/employee_service_app/src/main/java/com/techlabs/employeeapp/service/DepartmentApiClient.java
@@ -4,10 +4,13 @@ import com.techlabs.employeeapp.dto.DepartmentDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+//Before Eureka
+//@FeignClient(url = "http://localhost:8080", value = "department-service")
 
-@FeignClient(url = "http://localhost:8080/deptapp", value = "department-service")
+//After Eureka
+@FeignClient(name = "department-service")
 public interface DepartmentApiClient {
 
-    @GetMapping("/departments/{departmentId}")
+    @GetMapping("deptapp/departments/{departmentId}")
     public DepartmentDto getDepartment(@PathVariable Long departmentId);
 }
